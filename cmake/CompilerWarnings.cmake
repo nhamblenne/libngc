4f8e0d9976cee2526c6b9099a30795d447e5dfeb
Nicolas Ivan Hamblenne <nih@not-invented-here.org>
2021-07-11 10:21:01 +0200

    Compiler warnings for MSVC

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -2,6 +2,7 @@
 
 function(set_project_warnings project_name kind)
   option(WARNINGS_AS_ERRORS "Treat compiler warnings as errors" TRUE)
+  option(MSVC_WARNINGS_AS_ERRORS "Treat compiler warnings as errors" FALSE)
 
   set(CLANG_WARNINGS
       -Wall
@@ -22,15 +23,47 @@ function(set_project_warnings project_name kind)
           -Wduplicated-cond # warn if if / else chain has duplicated conditions
           -Wduplicated-branches # warn if if / else branches have duplicated code
           -Wlogical-op # warn about logical operations being used where bitwise were probably wanted
-          )
+  )
 
   set(SUNPRO_WARNINGS
-          -errtags)
+          -errtags
+  )
+
+  set(MSVC_WARNINGS
+          /W4 # Baseline reasonable warnings
+          /wd4996 # don't warn and suggest replacing standard function by proprietary one
+          /wd4146 # don't warn that unary operator on unsigned type gives a unsigned result
+          /w14242 # 'identifier': conversion from 'type1' to 'type1', possible loss of data
+          /w14254 # 'operator': conversion from 'type1:field_bits' to 'type2:field_bits', possible loss of data
+          /w14263 # 'function': member function does not override any base class virtual member function
+          /w14265 # 'classname': class has virtual functions, but destructor is not virtual instances of this class may not
+                  # be destructed correctly
+          /w14287 # 'operator': unsigned/negative constant mismatch
+          /we4289 # nonstandard extension used: 'variable': loop control variable declared in the for-loop is used outside
+                  # the for-loop scope
+          /w14296 # 'operator': expression is always 'boolean_value'
+          /w14311 # 'variable': pointer truncation from 'type1' to 'type2'
+          /w14545 # expression before comma evaluates to a function which is missing an argument list
+          /w14546 # function call before comma missing argument list
+          /w14547 # 'operator': operator before comma has no effect; expected operator with side-effect
+          /w14549 # 'operator': operator before comma has no effect; did you intend 'operator'?
+          /w14555 # expression has no effect; expected expression with side- effect
+          /w14619 # pragma warning: there is no warning number 'number'
+          /w14640 # Enable warning on thread un-safe static member initialization
+          /w14826 # Conversion from 'type1' to 'type_2' is sign-extended. This may cause unexpected runtime behavior.
+          /w14905 # wide string literal cast to 'LPSTR'
+          /w14906 # string literal cast to 'LPWSTR'
+          /w14928 # illegal copy-initialization; more than one user-defined conversion has been implicitly applied
+          /permissive- # standards conformance mode for MSVC compiler.
+  )
 
   if(WARNINGS_AS_ERRORS)
     set(CLANG_WARNINGS ${CLANG_WARNINGS} -Werror)
     set(GCC_WARNINGS ${GCC_WARNINGS} -Werror)
     set(SUNPRO_WARNINGS ${SUNPRO_WARNINGS} -errwarn)
+  endif()
+  if(MSVC_WARNINGS_AS_ERRORS)
+    set(MSVC_WARNINGS ${MSVC_WARNINGS} /WX)
   endif()
 
 
@@ -40,6 +73,8 @@ function(set_project_warnings project_name kind)
     set(PROJECT_WARNINGS ${GCC_WARNINGS})
   elseif(CMAKE_C_COMPILER_ID STREQUAL "SunPro")
     set(PROJECT_WARNINGS ${SUNPRO_WARNINGS})
+  elseif(MSVC)
+    set(PROJECT_WARNINGS ${MSVC_WARNINGS})
   else()
     message(AUTHOR_WARNING "No compiler warnings set for '${CMAKE_C_COMPILER_ID}' compiler.")
   endif()
